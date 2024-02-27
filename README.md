e3c2b6a0c9d335860bcf029aaac581a8c211c87e
peterN12g <peternguyensmhs@gmail.com>
2024-02-26 21:22:01 -0600

    initial layout commit

---
+++ README.md
@@ -0,0 +1,2 @@
+AnimeFinder
+Find an anime based on your search query.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,61 @@
+<script context="module">
+  import { GoogleGenerativeAI } from "@google/generative-ai";
+
+  let query = "";
+  const API_KEY = process.events.API_KEY;
+  const genAI = new GoogleGenerativeAI(API_KEY);
+
+  async function run() { 
+    const model = genAI.getGenerativeModel({ mode: "gemini-pro" });
+
+    const prompt = "Suggest an anime that " + query
+
+    const result = await model.generateContent(prompt);
+    const response = await result.response;
+    const text = response.text();
+    console.log(text);
+}
+
+</script>
+
+<html lang="en">
+  <body>
+    <h1>Welcome to AnimeFinder</h1>
+    <p>
+      Describe the type of anime you're in the mood for, and any specific elements you enjoy.
+      <input type="text" bind:value={query}>
+      <button class="Bind-Query" on:click={()=> run(prompt)}>Search</button>
+    </p>
+  </body>
+</html>
+
+<style>
+  body {
+    background-color: purple;
+  }
+
+  h1{
+    text-align: center;
+  }
+
+  p{
+    text-align: center;
+    font-size: 1.1em;
+  }
+
+  button.Bind-Query{
+    margin-left: auto;
+    margin-right: auto;
+    display: flex;
+    margin-top: 5px;
+  }
+
+  input{
+    display: flex;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5px;
+    width: 15%;
+  }
+</style>
+
