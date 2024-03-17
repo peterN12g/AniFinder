616a46f6f47f1988d5356f1d9d4cc247c9d35a69
peterN12g <peternguyensmhs@gmail.com>
2024-03-17 01:36:29 -0500

    400 error fix

--- src/routes/+server.js
+++ src/routes/+server.js
@@ -12,7 +12,7 @@ export async function GET({url}) {
   const prompt = url.searchParams.get("prompt");
   const genre = url.searchParams.get("genre")
   const text = await run(prompt,genre);
-  const titleMatch = text.match(/\*\*(?:\d+\.\d+\/10\s+)?(.*?)(?=\s*\*\*)/);
+  const titleMatch = text.match(/- (.*?)(?:\n|$)/);
   const title = titleMatch ? (titleMatch[1].trim() + " poster") : null;
   console.log(title)
    const imgLink = await getImage(title);
@@ -27,7 +27,7 @@ export async function GET({url}) {
 
 async function run(query,genre) { 
   const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro-001" });
-  const prompt = "Suggest ONLY one " + genre + " ANIME show with a title and ONLY 3 descriptive short bullet points in a vertical list format WITH the first bullet point the IMDB star rating like " + query;
+  const prompt = "Suggest ONLY one " + genre + " ANIME show with a title and ONLY 3 descriptive short bullet points in a vertical list format WITH the LAST bullet point being the IMDB star rating, like \"" + query + "\"";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
@@ -46,7 +46,7 @@ async function getImage(title) {
     q: title,
     searchType: "image",
     imgType: "photo",
-    siteSearch: "http://www.kylelambert.com",
+    siteSearch: "http://www.kylelambert.com OR https://lookaside.instagram.com",
     siteSearchFilter: "e",
   });
   let link = (result.data.items[0].link)
