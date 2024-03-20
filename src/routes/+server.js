b9c36a3adc94539cd4703f911d4d84e793d695bc
peterN12g <peternguyensmhs@gmail.com>
2024-03-20 12:10:38 -0500

    fix cloudflare deployment

--- src/routes/+server.js
+++ src/routes/+server.js
@@ -1,8 +1,8 @@
 //API call for Anime Information
 import { GoogleGenerativeAI } from "@google/generative-ai";
-import { API_KEY } from "$env/static/private";
-import { GOOGLE_API_KEY } from "$env/static/private"
-import { customsearch_v1 } from 'googleapis';
+import { API_KEY, CX, GOOGLE_API_KEY } from "$env/static/private";
+// import { GOOGLE_API_KEY } from "$env/static/private"
+// import { customsearch_v1 } from 'googleapis';
 
 const genAI = new GoogleGenerativeAI(API_KEY);
 
@@ -13,14 +13,15 @@ export async function GET({url}) {
   const text = await run(prompt,genre);
   const titleMatch = text.match(/- (.*?)(?:\n|$)/);
   const title = titleMatch ? (titleMatch[1].trim() + " poster") : null;
-   const imgLink = await getImage(title);
+   const imgLink = await getImageTest(title);
    const responseInfo = {
     text: text,
     imgLink: imgLink
    };
   //  logger.info("Successful API Call", JSON.stringify(responseInfo));
   return new Response(JSON.stringify(responseInfo));
-  } catch{
+  } catch (e) {
+    console.log(e)
     // logger.error("Failed API Call", error)
     return new Response("Error occurred", { status: 500 });
   }
@@ -38,23 +39,47 @@ async function run(query,genre) {
 
 // API call for Anime Image
 
-async function getImage(title) {
-  try{
-  const search = new customsearch_v1.Customsearch()
-  const result = await search.cse.list({
-    cx: "f5a3ae6b7bd4f4c69",
-    key: GOOGLE_API_KEY,
+// async function getImage(title) {
+//   try{
+//   const search = new customsearch_v1.Customsearch()
+//   const result = await search.cse.list({
+//     cx: "f5a3ae6b7bd4f4c69",
+//     key: GOOGLE_API_KEY,
+//     q: title,
+//     searchType: "image",
+//     imgType: "photo",
+//     siteSearch: "http://www.kylelambert.com OR https://lookaside.instagram.com",
+//     siteSearchFilter: "e",
+//   });
+//   let link = (result.data.items[0].link)
+//   return link;
+// } catch(error) {
+//   console.error("getImage error")
+//   throw error;
+//   // logger.error("API Image Call Error", error.message)
+// }}
+
+async function getImageTest(title){
+  const apiKey = GOOGLE_API_KEY;
+  const cx = CX;
+  const query = title;
+  const searchType = 'image';
+
+  // const url = 'https://www.googleapis.com/customsearch/v1?key='+apiKey+'&cx='+cx+':omuauf_lfve&q='+query+'&searchType=image&imgType=photo&siteSearch=http://www.kylelambert.com OR https://lookaside.instagram.com&siteSearchFilter=e';
+  const params = new URLSearchParams({
+    key: apiKey,
+    cx: cx,
     q: title,
     searchType: "image",
     imgType: "photo",
     siteSearch: "http://www.kylelambert.com OR https://lookaside.instagram.com",
     siteSearchFilter: "e",
   });
-  let link = (result.data.items[0].link)
-  // logger.info("Successful API Image Call" , link)
-  return link;
-} catch(error) {
-  // logger.error("API Image Call Error", error.message)
 
-}
+  let url = "https://www.googleapis.com/customsearch/v1?" + params.toString();
+
+  const result = await fetch(url);
+  const json = await result.json();
+  const linkTest = json.items[0].link;
+  return linkTest;
 }
